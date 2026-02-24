da8867dbc5333fdff3e2c90080851d3a2b668f41
nexiemadia <https://github.com/nexiemadia23>
2026-02-24 15:32:07 +0800

    Bootstrap Added

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -1,16 +1,16 @@
+  
 from django.contrib import admin  
 from .models import College, Program, Organization, Student, OrgMember  
   
 admin.site.register(College)  
 admin.site.register(Program)  
-admin.site.register(Organization)
+admin.site.register(Organization)    
 
-(Student)  
+@admin.register(Student)  
 class StudentAdmin(admin.ModelAdmin):  
     list_display = ("student_id", "lastname",  
                     "firstname", "middlename", "program")  
     search_fields = ("lastname", "firstname",) 
-
 admin.register(OrgMember)  
 class OrgMemberAdmin(admin.ModelAdmin):  
     list_display = ("student", "get_member_program", "organization",  
@@ -22,3 +22,4 @@ class OrgMemberAdmin(admin.ModelAdmin):
             return member.program  
         except Student.DoesNotExist:  
             return None 
+        
